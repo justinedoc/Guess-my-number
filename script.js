1d065d6fbbfdd9a0fc8d3bf75fa16e194b718f46
Onyiriuka Justin <richardjustin500@gmail.com>
2024-04-15 20:42:10 +0100

    Refactoring the code

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ const checkGuessBtn = document.querySelector('.check');
 const message = document.querySelector('.message');
 const score = document.querySelector('.score');
 const highScore = document.querySelector('.highscore');
-const defaultScore = Number(score.textContent);
-const randomNumber = Math.trunc(Math.random() * 20) + 1;
-
+// const defaultScore = Number(score.textContent);
+let randomNumber = Math.trunc(Math.random() * 20) + 1;
+let playing = true;
 let currentScore = 20;
 let currentHighScore = 0;
 score.textContent = currentScore;
@@ -29,35 +29,41 @@ const submitNumber = (input, btn) => {
 submitNumber('.guess', '.check');
 
 const guess = function (value) {
-  value = parseFloat(value);
-  if (!value || value > 20 || value < 1) {
-    message.textContent = 'Invalid guess!';
-  } else {
-    if (value === randomNumber) {
-      playerWins();
-    } else if (value > randomNumber) {
-      message.textContent = '📈 Too high!';
-      losing();
-    } else if (value < randomNumber) {
-      message.textContent = '📉 Too low!';
-      losing();
+  if (playing) {
+    value = parseFloat(value);
+    if (!value || value > 20 || value < 1) {
+      message.textContent = 'Invalid guess!';
+    } else {
+      if (value === randomNumber) {
+        playerWins();
+      } else if (value > randomNumber) {
+        message.textContent = '📈 Too high!';
+        losing();
+      } else if (value < randomNumber) {
+        message.textContent = '📉 Too low!';
+        losing();
+      }
     }
   }
 };
 
 const losing = () => {
-  if (currentScore <= 1) {
-    document.body.style.background = `red`;
-    message.textContent = `You Lose`;
-    score.textContent = 0;
-  } else {
-    currentScore = currentScore - 1;
-    score.textContent = currentScore;
+  if (playing) {
+    if (currentScore <= 1) {
+      document.body.style.background = `red`;
+      message.textContent = `You Lose`;
+      score.textContent = 0;
+    } else {
+      currentScore = currentScore - 1;
+      score.textContent = currentScore;
+    }
   }
 };
 
 const playerWins = () => {
   message.textContent = `🎉 Correct Number!`;
+
+  playing = false;
 
   document.querySelector('.number').textContent = randomNumber;
 
@@ -69,13 +75,16 @@ const playerWins = () => {
 };
 
 gameRestartBtn.addEventListener('click', () => {
+  playing = true;
+  randomNumber = Math.trunc(Math.random() * 6) + 1;
+  currentScore = 20;
   message.textContent = `Start guessing...`;
 
   document.querySelector('.number').textContent = '?';
 
   document.body.style.background = `#222`;
 
-  score.textContent = defaultScore;
+  score.textContent = 20;
 
   document.querySelector('.guess').value = '';
 });
